64dc57799d728d3b9067696de0e3e346346cb0f7
ShripalShah <shripalshah74@hotmail.com>
2018-02-22 16:58:29 -0800

    Nisacreations Code

---
+++ Nisacreation/SignIn.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Text;
+using System.Web.SessionState;
+namespace Nisacreation
+{
+    public partial class SignIn : System.Web.UI.Page
+    {
+        int count = 0;
+        String con;
+        OdbcConnection conx;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+
+            //Server.Transfer("WebSiteNisa.aspx", true);
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+
+            
+
+
+
+        }
+    }
+}
